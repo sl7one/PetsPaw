fbe2e957b53ea12c8278ded890624354226dc0d2
Slava <110696118+sl7one@users.noreply.github.com>
2023-08-28 14:54:00 +0300

    add layouts

---
+++ src/app/breeds/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.scss';
+import 'modern-normalize/modern-normalize.css';
+import type { Metadata } from 'next';
+import { Jost } from 'next/font/google';
+
+const jost = Jost({ subsets: ['latin'], display: 'swap' });
+
+export const metadata: Metadata = {
+   title: 'Breeds',
+   description: 'Breeds page',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+   return (
+      <html lang="en">
+         <body className={jost.className}>
+            {children}
+         </body>
+      </html>
+   );
+}
