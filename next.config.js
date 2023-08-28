9b1b30de115c21457c69ed8dbfb666f358044818
Slava <110696118+sl7one@users.noreply.github.com>
2023-08-28 15:15:31 +0300

    upd basePath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   output: "export",
-   images: { unoptimized: true }
+   output: 'export',
+   images: { unoptimized: true },
+   basePath: '/PetsPaw',
 };
 
 module.exports = nextConfig;
